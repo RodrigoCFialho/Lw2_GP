2f651659ee3ed4b0f8af21ac47468d5527336805
MariaHenriquesPouq <mariabri12388@gmail.com>
2024-05-31 13:46:21 +0100

    submarine movement
    and also offset das explosions and the limits

---
+++ Assignment2_Base/Assets/Scripts/SubmarineMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarineMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D myRigidBody2D;
+
+    [SerializeField]
+    private float speed = 3f;
+
+    [SerializeField]
+    private float wallDetectionTime = 0.1f;
+
+    [SerializeField]
+    private Transform wallDetectionPoint;
+
+    [SerializeField]
+    private Vector2 wallDetectionSize = new Vector2(0.1f, 1f);
+
+    [SerializeField]
+    private LayerMask wallLayerMask;
+
+    private void Start()
+    {
+        StartCoroutine(CheckWalls());
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidBody2D.velocity = new Vector2(transform.right.x * speed, myRigidBody2D.velocity.y);
+    }
+
+    private IEnumerator CheckWalls()
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(wallDetectionTime);
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.layerMask = wallLayerMask;
+        contactFilter.useLayerMask = true;
+
+        Collider2D[] results = new Collider2D[1];
+
+        while (true)
+        {
+            yield return waitTime;
+
+            if (Physics2D.OverlapBox(wallDetectionPoint.position, wallDetectionSize,
+                0f, contactFilter, results) > 0)
+            {
+                transform.right = -transform.right;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(wallDetectionPoint.position,
+                wallDetectionSize);
+    }
+}
